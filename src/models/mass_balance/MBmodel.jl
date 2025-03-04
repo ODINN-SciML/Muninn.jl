e9bc9a8ddff81e600884a677765fbd2b71c4471a
Jordi <jordi.bolibar@gmail.com>
2025-03-04 13:50:30 +0100

    Add/update docstrings

--- src/models/mass_balance/MBmodel.jl
+++ src/models/mass_balance/MBmodel.jl
@@ -1,33 +1,58 @@
 
 export TImodel1, TImodel2, MBmodel
 
-# Abstract type as a parent type for Mass Balance models
+"""
+    MBmodel <: AbstractModel
+
+An abstract type representing a mass balance model in the Muninn package. 
+This serves as a base type for all specific mass balance models, ensuring 
+they adhere to a common interface and can be used interchangeably within 
+the ODINN framework.
+"""
 abstract type MBmodel <: AbstractModel end
 
 ###############################################
 ########## TEMPERATURE-INDEX MODELS ###########
 ###############################################
 
-# Subtype structure for Temperature-Index Mass Balance model
+"""
+    TImodel <: MBmodel
+
+An abstract type representing a temperature index mass balance models within the ODINN framework. 
+This type serves as a parent type for more specialized mass balance models, ensuring they adhere to 
+a common interface defined by the `MBmodel` abstract type.
+"""
 abstract type TImodel <: MBmodel end
-# Temperature-index model with 1 melt factor
-# Make these mutable if necessary
+
+"""
+    TImodel1{F <: AbstractFloat}
+
+A structure representing a temperature index model with degree-day factor and accumulation factor.
+
+# Keyword arguments
+- `DDF::F`: Degree-day factor, which is a coefficient used to convert temperature into melt.
+- `acc_factor::F`: Accumulation factor, which is a coefficient used to adjust the accumulation of mass.
+
+# Type Parameters
+- `F`: A subtype of `AbstractFloat` representing the type of the factors.
+"""
 struct TImodel1{F <: AbstractFloat} <: TImodel
     DDF::F
     acc_factor::F
 end
 
 """
-    TImodel1(params::Sleipnir.Parameters;
-        DDF::Float64 = 7.0/1000.0,
-        acc_factor::Float64 = 1.0/1000.0
-        )
-Temperature-index model with a single degree-day factor.
+    TImodel1(params::Sleipnir.Parameters; DDF::F = 7.0/1000.0, acc_factor::F = 1.0/1000.0) where {F <: AbstractFloat}
 
-Keyword arguments
-=================
-    - `DDF`: Single degree-day factor, for both snow and ice.
-    - `acc_factor`: Accumulation factor
+Create a temperature index model with one degree-day factor (DDF) with the given parameters.
+
+# Arguments
+- `params::Sleipnir.Parameters`: The simulation parameters.
+- `DDF::F`: Degree-day factor (default is 7.0/1000.0).
+- `acc_factor::F`: Accumulation factor (default is 1.0/1000.0).
+
+# Returns
+- `TI1_model`: An instance of TImodel1 with the specified parameters.
 """
 function TImodel1(params::Sleipnir.Parameters;
             DDF::F = 7.0/1000.0,
@@ -40,7 +65,19 @@ function TImodel1(params::Sleipnir.Parameters;
     return TI1_model
 end
 
-# Temperature-index model with 2 melt factors
+"""
+    TImodel2{F <: AbstractFloat}
+
+A type representing a temperature-index model with parameters for snow and ice degree-day factors, and an accumulation factor.
+
+# Keyword arguments
+- `DDF_snow::F`: Degree-day factor for snow, which determines the melt rate of snow per degree above the melting point.
+- `DDF_ice::F`: Degree-day factor for ice, which determines the melt rate of ice per degree above the melting point.
+- `acc_factor::F`: Accumulation factor, which scales the accumulation of snow.
+
+# Type Parameters
+- `F`: A subtype of `AbstractFloat`, representing the numeric type used for the model parameters.
+"""
 struct TImodel2{F <: AbstractFloat} <: TImodel
     DDF_snow::F
     DDF_ice::F
@@ -48,18 +85,18 @@ struct TImodel2{F <: AbstractFloat} <: TImodel
 end
 
 """
-    TImodel2(params::Parameters;
-        DDF_snow::Float64 = 3.0/1000.0,
-        DDF_ice::Float64 = 6.0/1000.0,
-        acc_factor::Float64 = 1.0/1000.0
-        )
-Temperature-index model with two melt factors, for snow and ice.
+    TImodel2(params::Parameters; DDF_snow::F = 3.0/1000.0, DDF_ice::F = 6.0/1000.0, acc_factor::F = 1.0/1000.0) where {F <: AbstractFloat}
 
-Keyword arguments
-=================
-    - `DDF_snow`: Degree-day factor for snow.
-    - `DDF_ice`: Degree-day factor for ice.
-    - `acc_factor`: Accumulation factor
+Create a temperature-index model with two degree-day factors (TImodel2) for mass balance calculations.
+
+# Arguments
+- `params::Parameters`: The parameters object containing simulation settings.
+- `DDF_snow::F`: Degree-day factor for snow (default: 3.0/1000.0).
+- `DDF_ice::F`: Degree-day factor for ice (default: 6.0/1000.0).
+- `acc_factor::F`: Accumulation factor (default: 1.0/1000.0).
+
+# Returns
+- `TI2_model`: An instance of the TImodel2 with the specified parameters.
 """
 function TImodel2(params::Parameters;
             DDF_snow::F = 3.0/1000.0,
--- src/models/mass_balance/mass_balance_utils.jl
+++ src/models/mass_balance/mass_balance_utils.jl
@@ -1,22 +1,44 @@
 
 export compute_MB, MB_timestep!, MB_timestep
 
+
 """
     compute_MB(mb_model::TImodel1, climate_2D_period::Climate2Dstep)
-Compute the the mass balance given a mass balance model and a climate step.
 
-Keyword arguments
-=================
-    - `mb_model`: Mass balance model.
-    - `climate_2D_period`: Climate step.
+Compute the mass balance (MB) for a given mass balance model and climate period.
+
+# Arguments
+- `mb_model::TImodel1`: The mass balance model containing parameters such as accumulation factor (`acc_factor`) and degree-day factor (`DDF`).
+- `climate_2D_period::Climate2Dstep`: The climate data for a specific period, including snow accumulation (`snow`) and positive degree days (`PDD`).
+
+# Returns
+- A numerical array representing the computed mass balance, calculated as the difference between the product of the accumulation factor and snow, and the product of the degree-day factor and positive degree days.
 """
 function compute_MB(mb_model::TImodel1, climate_2D_period::Climate2Dstep)
     return ((mb_model.acc_factor .* climate_2D_period.snow) .- (mb_model.DDF .* climate_2D_period.PDD))
 end
 
+
 """
     MB_timestep(model::Model, glacier::G, step::F, t::F) where {F <: AbstractFloat, G <: AbstractGlacier}
-Retrieve the climate data, apply downscaling and compute the mass balance of a glacier for a specific time step.
+
+Calculate the mass balance (MB) for a glacier over a given timestep.
+
+# Keyword arguments
+- `model::Model`: The model containing mass balance parameters.
+- `glacier::G`: The glacier object containing climate and DEM data.
+- `step::F`: The timestep duration.
+- `t::F`: The current time.
+
+# Returns
+- `MB::Matrix{F}`: The computed mass balance matrix for the given timestep.
+
+# Details
+1. Computes the period between the current time `t` and the previous step `t - step`.
+2. Retrieves cumulative climate data for the specified period.
+3. Downscales the climate data to a 2D grid based on the glacier's DEM.
+4. Computes the mass balance using the downscaled climate data.
+
 """
 function MB_timestep(model::Model, glacier::G, step::F, t::F) where {F <: AbstractFloat, G <: AbstractGlacier}
     # First we get the dates of the current time and the previous step
@@ -28,9 +50,27 @@ function MB_timestep(model::Model, glacier::G, step::F, t::F) where {F <: Abstra
     return MB
 end
 
+
 """
     MB_timestep!(model::Model, glacier::G, step::F, t; batch_id::Union{Nothing, I} = nothing) where {I <: Integer, F <: AbstractFloat, G <: AbstractGlacier}
-Retrieve the climate data, apply downscaling and compute the mass balance of a glacier for a specific time step and possibly for a batch of iceflow models.
+
+Simulates a mass balance timestep for a given glacier model.
+
+# Arguments
+- `model::Model`: The glacier model containing iceflow and mass balance information.
+- `glacier::G`: The glacier object containing climate and DEM data.
+- `step::F`: The timestep duration.
+- `t`: The current time.
+- `batch_id::Union{Nothing, I}`: Optional batch identifier for simulations using Reverse Diff. Defaults to `nothing`.
+
+# Description
+This function performs the following steps:
+1. Computes the period from the previous timestep to the current time.
+2. Retrieves cumulative climate data for the specified period.
+3. Downscales the climate dataset to a 2D grid based on the glacier's DEM.
+4. Computes the mass balance for the glacier and updates the model's iceflow mass balance.
+
+If `batch_id` is provided, the function updates the mass balance for the specified batch; otherwise, it updates the mass balance for the entire model.
 """
 function MB_timestep!(model::Model, glacier::G, step::F, t; batch_id::Union{Nothing, I} = nothing) where {I <: Integer, F <: AbstractFloat, G <: AbstractGlacier}
     # First we get the dates of the current time and the previous step
