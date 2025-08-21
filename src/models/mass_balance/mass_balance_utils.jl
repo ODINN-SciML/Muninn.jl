3172d137c7e333790387046d86a6ad5c3f824d8e
Alban Gossard <alban.paul.gossard@gmail.com>
2025-08-21 16:59:51 +0200

    changes to support Enzyme differentiation of MB

--- src/models/mass_balance/mass_balance_utils.jl
+++ src/models/mass_balance/mass_balance_utils.jl
@@ -41,9 +41,8 @@ Calculate the mass balance (MB) for a glacier over a given timestep.
 
 """
 function MB_timestep(model::Model, glacier::G, step::F, t::F) where {F <: AbstractFloat, G <: AbstractGlacier}
-    # First we get the dates of the current time and the previous step
-    period = partial_year(Day, t - step):Day(1):partial_year(Day, t)
-    get_cumulative_climate!(glacier.climate, period)
+    get_cumulative_climate!(glacier.climate, t, step)
+
     # Convert climate dataset to 2D based on the glacier's DEM
     climate_2D_step = downscale_2D_climate(glacier.climate.climate_step, glacier.S, glacier.Coords)
     MB::Matrix{F} = compute_MB(model.mass_balance, climate_2D_step)
@@ -62,7 +61,6 @@ Simulates a mass balance timestep for a given glacier model.
 - `glacier::G`: The glacier object containing climate and DEM data.
 - `step::F`: The timestep duration.
 - `t`: The current time.
-- `batch_id::Union{Nothing, I}`: Optional batch identifier for simulations using Reverse Diff. Defaults to `nothing`.
 
 # Description
 This function performs the following steps:
@@ -70,14 +68,9 @@ This function performs the following steps:
 2. Retrieves cumulative climate data for the specified period.
 3. Downscales the climate dataset to a 2D grid based on the glacier's DEM.
 4. Computes the mass balance for the glacier and updates the model's iceflow mass balance.
-
-If `batch_id` is provided, the function updates the mass balance for the specified batch; otherwise, it updates the mass balance for the entire model.
 """
 function MB_timestep!(cache, model::Model, glacier::G, step::F, t) where {F <: AbstractFloat, G <: AbstractGlacier}
-    # First we get the dates of the current time and the previous step
-    period = partial_year(Day, t - step):Day(1):partial_year(Day, t)
-
-    get_cumulative_climate!(glacier.climate, period)
+    get_cumulative_climate!(glacier.climate, t, step)
 
     # Convert climate dataset to 2D based on the glacier's DEM
     downscale_2D_climate!(glacier)
