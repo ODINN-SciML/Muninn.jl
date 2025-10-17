d15dfdc72c22ea825916650d47058a537471ac38
Alban Gossard <alban.paul.gossard@gmail.com>
2025-10-17 10:50:02 +0200

    propagate Sleipnir changes (#39)

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,6 +17,8 @@ include("MB.jl")
 # Activate to avoid GKS backend Plot issues in the JupyterHub
 ENV["GKSwstype"]="nul"
 
-@testset "Construct TI models by default" TI_creation_default_test()
-@testset "Construct TI models with input values" TI_creation_values_test()
-@testset "Apply MB model" apply_MB_test()
+@testset "Run all tests" begin
+    @testset "Construct TI models by default" TI_creation_default_test()
+    @testset "Construct TI models with input values" TI_creation_values_test()
+    @testset "Apply MB model" apply_MB_test()
+end
